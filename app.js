6223d12d021e9bae45d0f2613ad1c0cf34e94322
superjavamanster <superjavamanster@gmail.com>
2022-01-18 21:48:37 +0800

    add axios in html

--- app.js
+++ app.js
@@ -12,11 +12,12 @@ createApp({
     login() {
       const api = 'https://vue3-course-api.hexschool.io/v2/admin/signin';
       axios.post(api, this.user).then((response) => {
-        // const { token, expired } = response.data;
-        // // 寫入 cookie token
-        // // expires 設置有效時間
-        // document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
-        // window.location = 'products.html';
+        // 測試API是否連接 ok 使用 console.log(response); 
+        const { token, expired } = response.data;
+        // 寫入 cookie token
+        // expires 設置有效時間
+        document.cookie = `hexToken=${token};expires=${new Date(expired)}; path=/`;
+        window.location = 'week2-products.html';
       }).catch((error) => {
         alert(error.data.message);
       });
