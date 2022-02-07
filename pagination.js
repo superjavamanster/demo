1c85e215cbf1e4b9ca5d63babaca946ca4fe5237
superjavamanster <superjavamanster@gmail.com>
2022-02-07 16:51:33 +0800

    fix_page樣式

--- pagination.js
+++ pagination.js
@@ -1,7 +1,7 @@
 export default {
-   props: ['pages'], // 外層傳入內層-傳入page資訊 還有第10,11行  (作業看到26:30分)
+   props: ['pages'], // 外層傳入內層-傳入page資訊 還有第10,11行  (作業看到30:21分)
    // 第6,11,12,13,14,16行 套用bootstrap pagination樣式
-   // 頁面切換事件 @
+   // 頁面切換事件 第16行@click="$emit('get-data', page)">{{page}}</a> 
    template: `<nav aria-label="Page navigation example">
    <ul class="pagination">
      <li class="page-item" :class="{ disabled: !pages.has_pre}">
@@ -12,7 +12,8 @@ export default {
      <li class="page-item" 
      :class="{ active: page === pages.current_page}"
      v-for="page in pages.total_pages" :key="page + 'page'">  
-     <a class="page-link" href="#">{{page}}</a> 
+     <a class="page-link" href="#"
+     @click="$emit('get-data', page)">{{page}}</a> 
      </li>
      <li class="page-item" :class="{ disabled: !pages.has_next}">
        <a class="page-link" href="#" aria-label="Next">
